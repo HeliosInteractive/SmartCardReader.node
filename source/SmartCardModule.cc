01af216a5b0e15dbc8b74103396e073f41531510
Sepehr Laal <sepehr.laal@gmail.com>
2016-07-13 13:44:15 -0700

    export wrapper into its own native module

---
+++ source/SmartCardModule.cc
@@ -0,0 +1,24 @@
+#include "SmartCardWrapper.hh"
+
+#include <nan.h>
+
+namespace helios
+{
+
+NAN_MODULE_INIT(ModuleInit)
+{
+    auto cls = "SmartCardReader";
+    auto tpl = Nan::New<v8::FunctionTemplate>(SmartCardWrapper::New);
+
+    tpl->SetClassName(Nan::New(cls).ToLocalChecked());
+    tpl->InstanceTemplate()->SetInternalFieldCount(1);
+
+    SetPrototypeMethod(tpl, "poll", SmartCardWrapper::Poll);
+    SetPrototypeMethod(tpl, "query", SmartCardWrapper::Query);
+
+    Nan::Set(target, Nan::New(cls).ToLocalChecked(), tpl->GetFunction());
+};
+
+}
+
+NODE_MODULE(SmartCardReader, helios::ModuleInit)
